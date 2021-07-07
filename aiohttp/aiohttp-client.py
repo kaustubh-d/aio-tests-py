7a2f6f654f6c72ded5720a250867fa3d786c837d
kaustubh-d <2116373+kaustubh-d@users.noreply.github.com>
2021-07-07 12:41:32 +0530

    Fix aiohttp client to compute avg req time correctly.

--- aiohttp/aiohttp-client.py
+++ aiohttp/aiohttp-client.py
@@ -13,16 +13,10 @@ def print_report(total_count, total_time_ms):
     print("-----------------------------------------\n")
 
 async def async_put_api(session):
-    start_time = time.perf_counter()
-
     resp = await session.put("http://localhost/hello", data="Hello world!")
 
-    end_time = time.perf_counter()
-    elapsed_time_ms = int(round((end_time - start_time) * 1000))
-
     assert resp.status == 200
-
-    return {"elapsed_time": elapsed_time_ms}
+    return
 
 # Test case 1: Runs each request in sequence.
 async def test_case_seq_reqs(total_count):
@@ -31,8 +25,11 @@ async def test_case_seq_reqs(total_count):
 
     total_time_ms = 0
     for i in range(total_count):
-        resp = await async_put_api(client_session)
-        total_time_ms += resp["elapsed_time"]
+        start_time = time.perf_counter()
+        await async_put_api(client_session)
+        end_time = time.perf_counter()
+        elapsed_time_ms = int(round((end_time - start_time) * 1000))
+        total_time_ms += elapsed_time_ms
 
     print_report(total_count, total_time_ms)
 
@@ -45,18 +42,19 @@ async def test_case_parallel(total_count, max_conn):
     connector = aiohttp.TCPConnector(limit=max_conn, limit_per_host=max_conn)
     client_session = aiohttp.ClientSession(connector=connector)
 
-    total_time_ms = 0
     task_list = []
     for i in range(total_count):
         task = asyncio.ensure_future(async_put_api(client_session))
         task_list.append(task)
 
-    responses = await asyncio.gather(*task_list)
+    # Launch the operations.
+    start_time = time.perf_counter()
+    await asyncio.gather(*task_list)
+    end_time = time.perf_counter()
+    elapsed_time_ms = int(round((end_time - start_time) * 1000))
 
-    for response in responses:
-        total_time_ms += response["elapsed_time"]
+    print_report(total_count, elapsed_time_ms)
 
-    print_report(total_count, total_time_ms)
 
     await client_session.close()
 
